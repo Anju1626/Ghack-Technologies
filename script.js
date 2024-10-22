037541b9ae0475f21b660278565897478c1c152b
Anju1626 <officialanju1103@gmail.com>
2024-10-23 00:16:43 +0530

    major fixes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Character data
 const characters = [
-    { name: "Persephone", image: "images/persephone.jpg", details: "Goddess of spring." },
-    { name: "Hades", image: "images/hades.jpg", details: "God of the underworld." },
-    { name: "ZEUS", image: "images/dionysus.jpg", details: "God of wine and festivity." },
-    { name: "Posiedon", image: "images/athena.jpg", details: "Goddess of wisdom." },
-    { name: "Eros", image: "images/persephone.jpg", details: "Goddess of spring." },
-    { name: "Hermes", image: "images/hades.jpg", details: "God of the underworld." },
-    { name: "Apollo", image: "images/dionysus.jpg", details: "God of wine and festivity." },
-    { name: "Atermis", image: "images/athena.jpg", details: "Goddess of wisdom." },
-    { name: "Demeter", image: "images/persephone.jpg", details: "Goddess of spring." },
-    { name: "Ares", image: "images/hades.jpg", details: "God of the underworld." },
+    { name: "Persephone", image: "image/persephone.jpg", details: "Goddess of spring." },
+    { name: "Hades", image: "image/hades.jpg", details: "God of the underworld." },
+    { name: "ZEUS", image: "image/dionysus.jpg", details: "God of wine and festivity." },
+    { name: "Posiedon", image: "image/athena.jpg", details: "Goddess of wisdom." },
+    { name: "Eros", image: "image/persephone.jpg", details: "Goddess of spring." },
+    { name: "Hermes", image: "image/hades.jpg", details: "God of the underworld." },
+    { name: "Apollo", image: "image/dionysus.jpg", details: "God of wine and festivity." },
+    { name: "Atermis", image: "image/athena.jpg", details: "Goddess of wisdom." },
+    { name: "Demeter", image: "image/persephone.jpg", details: "Goddess of spring." },
+    { name: "Ares", image: "image/hades.jpg", details: "God of the underworld." },
     // Add more characters as needed
 ];
 
@@ -30,15 +30,15 @@ function displayCharacters() {
 
 // Carousel data
 const manhwas = [
-    { title: "Leviathan", image: "images/leviathan.jpg" },
-    { title: "Tower of God", image: "images/tower-of-god.jpg" },
-    { title: "Levelling up the gods", image: "images/levelling-up-the-gods.jpg" },
-    { title: "Existence", image: "images/existence.jpg" },
-    { title: "The Breaker", image: "images/the breaker.jpg" },
-    { title: "The First Hunter", image: "images/the-first-hunter.jpg" },
-    { title: "Noblesse", image: "images/noblesse.jpg" },
-    { title: "Ultimate Outcast", image: "images/ultimate-outcast.jpg" },
-    { title: "Unholy Blood", image: "images/unholy-blood.jpg" },
+    { title: "Leviathan", image: "image/leviathan.jpg" },
+    { title: "Tower of God", image: "image/tower-of-god.jpg" },
+    { title: "Levelling up the gods", image: "image/levelling-up-the-gods.jpg" },
+    { title: "Existence", image: "image/existence.jpg" },
+    { title: "The Breaker", image: "image/the breaker.jpg" },
+    { title: "The First Hunter", image: "image/the-first-hunter.jpg" },
+    { title: "Noblesse", image: "image/noblesse.jpg" },
+    { title: "Ultimate Outcast", image: "image/ultimate-outcast.jpg" },
+    { title: "Unholy Blood", image: "image/unholy-blood.jpg" },
     { title: "dice-the Club that changes everything", image: "image/dice-the club that changes everything.jpg" },
     // Add more manhwas as needed
 ];
